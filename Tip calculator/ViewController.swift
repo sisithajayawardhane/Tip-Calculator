6f81b45c850628574722556e384ba5bc738d7515
Sisitha Jayawardhane <sisithajayawardhane@gmail.com>
2020-05-06 08:38:19 +0530

    Interface done for i phone 8

---
+++ Tip calculator/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Tip calculator
+//
+//  Created by sisitha jayawardhane on 5/2/20.
+//  Copyright © 2020 Sisitha Jayawardhane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var Segment_view: UISegmentedControl!
+    @IBOutlet weak var SegmentView: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
+
